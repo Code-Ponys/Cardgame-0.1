b90914165cfcfc75a3e0cc59f8812d95ce046d97
CodeFoxByChojo <playhadde@gmail.com>
2017-07-25 17:56:42 +0200

    added empty files for special cards

---
+++ Assets/Scripts/Cards/BlockCard.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockCard : Card {
+
+    public string blockDirection;
+    public int x { get; set; }
+    public int y { get; set; }
+    public int team { get; set; }
+    public int state { get; set; }
+
+    protected Block? GetBlockDirection() {
+        switch (blockDirection) {
+            default:
+                return null;
+            case "up":
+                return Block.up;
+            case "down":
+                return Block.down;
+            case "left":
+                return Block.left;
+            case "right":
+                return Block.right;
+        }
+    }
+
+    protected override CardID? GetType() {
+        return CardID.Blockcard;
+    }
+}

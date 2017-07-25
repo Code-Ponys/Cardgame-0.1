4872b38cb9a3d1db461c4be48ef3813431de3e18
CodeFoxByChojo <playhadde@gmail.com>
2017-07-25 17:57:45 +0200

    minor changes

---
+++ Assets/Scripts/Defines.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+[System.Serializable]
+public class FieldProperties : System.Object {
+    public int _size;
+}
+
+public enum CardID { Startpoint, Pointcard, Blankcard,
+    Doublecard, Blockcard, Deletecard,
+    Burncard, Infernocard, Changecard,
+    Cancercard, HotPotatoe, Nukecard,
+    Vortexcard, Anchorcard, Sufflecard }
+public enum Team {
+    system, blau, rot
+}
+public enum Block {
+    up, right, left, down
+}
+
+public enum State {
+    deck, hand, field
+}
+public class Materials {
+    //Material transparent = new Material();
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    List<Card> Deck = new List<Card>();
+
+    List<Card> Hand = new List<Card>();
+
+    // Use this for initialization
+    void Start() {
+        for (int x = 0; x < 15; x++) {
+            Deck.Add(new BlankCard() { x = 0, y = 0, team = 0, state = 0 });
+            Deck.Add(new PointCard() { x = 0, y = 0, team = 0, state = 0 });
+        }
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+}

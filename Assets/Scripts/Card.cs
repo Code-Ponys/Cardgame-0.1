47a3e86712a62fe31f3eacebe0c01f7b2198a350
CodeFoxByChojo <playhadde@gmail.com>
2017-07-25 17:57:37 +0200

    added abstract Card

---
+++ Assets/Scripts/Card.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Card : MonoBehaviour {
+
+    int state;
+    int team;
+
+    protected abstract new CardID? GetType();
+
+    protected void CardShow() {
+
+    }
+
+    protected virtual Team? GetTeam() {
+        switch (team) {
+            default:
+                return null;
+            case 0:
+                return Team.system;
+            case 1:
+                return Team.blau;
+            case 2:
+                return Team.rot;
+        }
+    }
+
+    protected virtual State? GetState() {
+        switch (state) {
+            default:
+                return null;
+            case 0:
+                return State.deck;
+            case 1:
+                return State.hand;
+            case 2:
+                return State.field;
+
+        }
+    }
+}

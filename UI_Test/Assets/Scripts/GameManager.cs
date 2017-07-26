160591a87ceeb9b877e92e6a7ee9c2b38c68b3ea
LaraPaul <lapaul@stud.mediadesign.de>
2017-07-26 09:29:24 +0200

    Ui Scenes

---
+++ UI_Test/Assets/Scripts/GameManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    private void Update()
+    {
+        
+    }
+
+    public void ChangeToScene(string SceneToChangeTo)
+    {
+        SceneManager.LoadScene(SceneToChangeTo);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    
+}
+
+ 
---
+++ UI_Test/Assets/Scripts/PanelMove.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PanelMove : MonoBehaviour {
+
+    private void Start() {
+        transform.position = new Vector3(11, 0, 0);
+    }
+
+    public void OnMouseEnter() {
+        Vector3 point = new Vector3(11, 0, 0);
+        Vector3 goal = new Vector3(6, 0, 0);
+        Physics.queriesHitTriggers = true;
+        point = this.transform.position;
+        if (point != goal) {
+            transform.Translate(-5, 0, 0);
+        }
+    }
+
+    public void OnMouseExit() {
+        Physics.queriesHitTriggers = true;
+        transform.Translate(2.5f, 0, 0);
+    }
+   
+}

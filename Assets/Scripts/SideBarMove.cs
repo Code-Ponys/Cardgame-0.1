4f627e974dd8949b6864f4b17fc013913dcf0b68
CodeFoxByChojo <playhadde@gmail.com>
2017-07-31 15:48:24 +0200

    added and fixed sidebar scroll script

---
+++ Assets/Scripts/SideBarMove.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SideBarMove : MonoBehaviour {
+
+    private void Start() {
+        RectTransform rectTransform = GetComponent<RectTransform>();
+        rectTransform.anchoredPosition = new Vector3(240, 0, 0);
+    }
+
+    public void MovePanelOut() {
+        print("MovePanelOut");
+        Vector3 goal = new Vector3(240, 0, 0);
+        Vector3 point = new Vector3(-300, 0, 0);
+        Physics.queriesHitTriggers = true;
+        if (point != goal) {
+            RectTransform var = GetComponent<RectTransform>();
+            var.anchoredPosition = goal;
+            print("MovePanelOut Moved");
+        }
+    }
+
+    public void MovePanelIn() {
+        print("MovePanelIn");
+        Vector3 point = new Vector3(240, 0, 0);
+        Vector3 goal = new Vector3(-300, 0, 0);
+        Physics.queriesHitTriggers = true;
+        if (point != goal) {
+            RectTransform var = GetComponent<RectTransform>();
+            var.anchoredPosition = goal;
+            print("MovePanelIn Moved");
+        }
+    }
+}

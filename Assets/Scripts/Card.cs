38ac1b55de426030bf8079293583b0b8139201db
CodeFoxByChojo <playhadde@gmail.com>
2017-08-02 18:34:36 +0200

    cleaned up

--- Assets/Scripts/Card.cs
+++ Assets/Scripts/Card.cs
@@ -5,41 +5,8 @@ using Cards;
 
 public abstract class Card : MonoBehaviour {
 
-    public int state;
-    public int team;
+    public State state;
+    public Team team;
     public int x;
     public int y;
-
-    protected abstract new CardID? GetType();
-
-    protected void CardShow() {
-
-    }
-
-    protected virtual Team? GetTeam() {
-        switch (team) {
-            default:
-                return null;
-            case 0:
-                return Team.system;
-            case 1:
-                return Team.blau;
-            case 2:
-                return Team.rot;
-        }
-    }
-
-    protected virtual State? GetState() {
-        switch (state) {
-            default:
-                return null;
-            case 0:
-                return State.deck;
-            case 1:
-                return State.hand;
-            case 2:
-                return State.field;
-        }
-    }
-    
 }

0347ab72b377604512820f1924f61d0e200a28d0
CodeFoxByChojo <playhadde@gmail.com>
2017-08-02 18:33:51 +0200

    moved CamerController to CameraManager

---
+++ Assets/CameraManager.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Cards;
+using System;
+
+
+public class CameraManager : MonoBehaviour {
+
+    public Camera MainCam;
+    //public Camera BGCam;
+    public Field Field;
+
+    private int max_x = 0;
+    private int min_x = 0;
+    private int max_y = 0;
+    private int min_y = 0;
+    private int cameraSize = 10;
+
+    // Use this for initialization
+    void Start() {
+        
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+    public Vector3 GetCenter() {
+
+        float center_x = (((min_x * -1) + max_x) / 2) + min_x;
+        float center_y = (((min_y * -1) + max_y) / 2) + min_y;
+        Vector3 result = new Vector3((float)center_x, (float)center_y, -10);
+        return result;
+    }
+
+    public int GetCameraSize() {
+        int size_x = max_x - min_x;
+        int size_y = max_y - min_y;
+
+        int size = Math.Max(size_x, size_y)+8;
+        return Math.Max(4, size / 2);
+    }
+
+    public void CenterCamera(int x, int y) {
+        CalculateSize(x,y);
+        MainCam.transform.position = GetCenter();
+        MainCam.orthographicSize = GetCameraSize();
+        //BGCam.orthographicSize = GetCameraSize();
+    }
+
+    public void CalculateSize(int x, int y) {
+
+        if (x > max_x) {
+            max_x = x;
+        }
+        if (x < min_x) {
+            min_x = x;
+        }
+        if (y > max_y) {
+            max_y = y;
+        }
+        if (y < min_y) {
+            min_y = y;
+        }
+    }
+}

3320568c962e9a574d4405866869a56783a76fbb
CodeFoxByChojo <playhadde@gmail.com>
2017-08-02 18:36:30 +0200

    some small stuff

---
+++ Assets/Scripts/Indicator.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Cards {
+    public class Indicator : MonoBehaviour {
+
+        public bool blocked = true;
+
+        // Use this for initialization
+        void Start() {
+        }
+
+        // Update is called once per frame
+        void Update() {
+
+        }
+        public bool isBlocked() {
+            return blocked;
+        }
+    }
+}
---
+++ Assets/Scripts/Slave.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+static public class Slave {
+    static public string GetImagePath(CardID card) {
+        switch (card) {
+            default:
+                return "cards/pf_ErrorCard";
+            case CardID.Blankcard:
+                return "cards/pf_Blankcard";
+            case CardID.Pointcard:
+                return "cards/pf_Pointcard";
+            case CardID.Startpoint:
+                return "cards/pf_Startpoint";
+            case CardID.Blockcard:
+                return "cards/pf_Blockcard";
+            case CardID.Indicator:
+                return "emptycards/pf_black";
+            case CardID.Indicatorred:
+                return "emptycards/pf_red";
+        }
+    }
+}

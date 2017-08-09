059251a02b97dc9ec69d702b67f67fe56c84659c
CodeFoxByChojo <playhadde@gmail.com>
2017-08-09 17:44:19 +0200

    Prepared for more Specialcards

--- Assets/Scripts/Slave.cs
+++ Assets/Scripts/Slave.cs
@@ -19,6 +19,28 @@ static public class Slave {
                 return "emptycards/pf_red";
             case CardID.Startpoint:
                 return "cards/pf_Startpoint";
+            case CardID.Doublecard:
+                return "cards/" + team + "/pf_Doublecard";
+            case CardID.Deletecard:
+                return "cards/" + team + "/pf_Deletecard";
+            case CardID.Burncard:
+                return "cards/" + team + "/pf_Burncard";
+            case CardID.Infernocard:
+                return "cards/" + team + "/pf_Infernocard";
+            case CardID.Changecard:
+                return "cards/" + team + "/pf_Changecard";
+            case CardID.Cancercard:
+                return "cards/" + team + "/pf_Cancercard";
+            case CardID.HotPotatoe:
+                return "cards/" + team + "/pf_HotPotatoe";
+            case CardID.Nukecard:
+                return "cards/" + team + "/pf_Nukecard";
+            case CardID.Vortexcard:
+                return "cards/" + team + "/pf_Vortexcard";
+            case CardID.Anchorcard:
+                return "cards/" + team + "/pf_Anchorcard";
+            case CardID.Shufflecard:
+                return "cards/" + team + "/pf_Shufflecard";
         }
     }
 
@@ -34,6 +56,28 @@ static public class Slave {
                 return "cards/Startpoint";
             case CardID.Blockcard:
                 return "cards/Blockcard";
+            case CardID.Doublecard:
+                return "cards/Doublecard";
+            case CardID.Deletecard:
+                return "cards/Deletecard";
+            case CardID.Burncard:
+                return "cards/Burncard";
+            case CardID.Infernocard:
+                return "cards/Infernocard";
+            case CardID.Changecard:
+                return "cards/Changecard";
+            case CardID.Cancercard:
+                return "cards/Cancercard";
+            case CardID.HotPotatoe:
+                return "cards/HotPotatoe";
+            case CardID.Nukecard:
+                return "cards/Nukecard";
+            case CardID.Vortexcard:
+                return "cards/Vortexcard";
+            case CardID.Anchorcard:
+                return "cards/Anchorcard";
+            case CardID.Shufflecard:
+                return "cards/pf_Shufflecard";
         }
     }
 }

102106d015604d75d4f96680bced64faeb037b9d
CodeFoxByChojo <playhadde@gmail.com>
2017-08-11 17:48:55 +0200

    refactoring

--- Assets/Scripts/Cards/DeleteCard.cs
+++ Assets/Scripts/Cards/DeleteCard.cs
@@ -8,7 +8,8 @@ namespace Cards {
         // Use this for initialization
         void Start() {
             GameObject F = GameObject.Find("Field");
-            if (GameObject.Find("Pointcard " + x + "," + y) != null) {
+            GameObject Card = GameObject.Find("Card " + x + "," + y);
+            if (Card != null) {
                 for (int i = 0; i < F.GetComponent<Field>().cardsOnField.Count; i++) {
                     if (F.GetComponent<Field>().cardsOnField[i].GetComponent<Card>().x == x
                         && F.GetComponent<Field>().cardsOnField[i].GetComponent<Card>().y == y) {
@@ -16,63 +17,28 @@ namespace Cards {
                         break;
                     }
                 }
-                Destroy(GameObject.Find("Pointcard " + x + "," + y));
-            }
-            if (GameObject.Find("Blankcard " + x + "," + y) != null) {
-                for (int i = 0; i < F.GetComponent<Field>().cardsOnField.Count; i++) {
-                    if (F.GetComponent<Field>().cardsOnField[i].GetComponent<Card>().x == x
-                        && F.GetComponent<Field>().cardsOnField[i].GetComponent<Card>().y == y) {
-                        F.GetComponent<Field>().cardsOnField.RemoveAt(i);
-                        break;
+                if (Card.GetComponent<Card>().cardid == CardID.Blockcard) {
+                    Block blockdirection = GameObject.Find("Card " + x + "," + y).GetComponent<BlockCard>().blockDirection;
+                    switch (blockdirection) {
+                        case Block.right:
+                            GameObject.Find("FieldIndicator " + (x + 1) + "," + y).GetComponent<Indicator>().indicatorState = IndicatorState.unreachable;
+                            break;
+                        case Block.left:
+                            GameObject.Find("FieldIndicator " + (x - 1) + "," + y).GetComponent<Indicator>().indicatorState = IndicatorState.unreachable;
+                            break;
+                        case Block.up:
+                            GameObject.Find("FieldIndicator " + x + "," + (y + 1)).GetComponent<Indicator>().indicatorState = IndicatorState.unreachable;
+                            break;
+                        case Block.down:
+                            GameObject.Find("FieldIndicator " + x + "," + (y - 1)).GetComponent<Indicator>().indicatorState = IndicatorState.unreachable;
+                            break;
                     }
                 }
-                Destroy(GameObject.Find("Blankcard " + x + "," + y));
+                Destroy(GameObject.Find("Card " + x + "," + y));
             }
-            for (int i = 0; i < F.GetComponent<Field>().cardsOnField.Count; i++) {
-                if (F.GetComponent<Field>().cardsOnField[i].GetComponent<Card>().x == x
-                    && F.GetComponent<Field>().cardsOnField[i].GetComponent<Card>().y == y) {
-                    F.GetComponent<Field>().cardsOnField.RemoveAt(i);
-                        break;
-                }
-            }
-            if (GameObject.Find("Anchorcard " + x + "," + y) != null) {
-                for (int i = 0; i < F.GetComponent<Field>().cardsOnField.Count; i++) {
-                    if (F.GetComponent<Field>().cardsOnField[i].GetComponent<Card>().x == x
-                        && F.GetComponent<Field>().cardsOnField[i].GetComponent<Card>().y == y) {
-                        F.GetComponent<Field>().cardsOnField.RemoveAt(i);
-                        break;
-                    }
-                }
-                Destroy(GameObject.Find("Anchorcard " + x + "," + y));
-            }
-            if (GameObject.Find("Blockcard " + x + "," + y) != null) {
-                Block blockdirection = GameObject.Find("Blockcard " + x + "," + y).GetComponent<BlockCard>().blockDirection;
-                for (int i = 0; i < F.GetComponent<Field>().cardsOnField.Count; i++) {
-                    if (F.GetComponent<Field>().cardsOnField[i].GetComponent<Card>().x == x
-                        && F.GetComponent<Field>().cardsOnField[i].GetComponent<Card>().y == y) {
-                        F.GetComponent<Field>().cardsOnField.RemoveAt(i);
-                        break;
-                    }
-                }
-                Destroy(GameObject.Find("Blockcard " + x + "," + y));
-                switch (blockdirection) {
-                    case Block.right:
-                        GameObject.Find("FieldIndicator " + (x + 1) + "," + y).GetComponent<Indicator>().indicatorState = IndicatorState.unreachable;
-                        break;
-                    case Block.left:
-                        GameObject.Find("FieldIndicator " + (x - 1) + "," + y).GetComponent<Indicator>().indicatorState = IndicatorState.unreachable;
-                        break;
-                    case Block.up:
-                        GameObject.Find("FieldIndicator " + x + "," + (y + 1)).GetComponent<Indicator>().indicatorState = IndicatorState.unreachable;
-                        break;
-                    case Block.down:
-                        GameObject.Find("FieldIndicator " + x + "," + (y - 1)).GetComponent<Indicator>().indicatorState = IndicatorState.unreachable;
-                        break;
-                }
-            }
-            Destroy(GameObject.Find("Deletecard " + x + "," + y));
+            Destroy(GameObject.Find("DeleteCard " + x + "," + y));
         }
-        
+
 
         // Update is called once per frame
         void Update() {

58f720fad49851823105860a5c0077cc8c0904f9
CodeFoxByChojo <playhadde@gmail.com>
2017-08-14 10:54:59 +0200

    added GetCardName()

--- Assets/Scripts/Slave.cs
+++ Assets/Scripts/Slave.cs
@@ -5,9 +5,9 @@ using UnityEngine;
 static public class Slave {
     static public string GetImagePathPf(CardID card, Team team) {
         if (card != CardID.FieldIndicator
-            || card != CardID.Indicatorred
-            || card != CardID.Startpoint
-            || card != CardID.CardIndicator) {
+            && card != CardID.FieldIndicatorRed
+            && card != CardID.Startpoint
+            && card != CardID.CardIndicator) {
             return "cards/" + team + "/pf_" + card;
         }
         switch (card) {
@@ -15,7 +15,7 @@ static public class Slave {
                 return "cards/" + team + "/pf_Errorcard";
             case CardID.FieldIndicator:
                 return "emptycards/pf_black";
-            case CardID.Indicatorred:
+            case CardID.FieldIndicatorRed:
                 return "emptycards/pf_red";
             case CardID.Startpoint:
                 return "cards/pf_Startpoint";
@@ -58,8 +58,63 @@ static public class Slave {
                 return "cards/" + team + "/Anchorcard";
             case CardID.Shufflecard:
                 return "cards/" + team + "/Shufflecard";
+            case CardID.CardIndicator:
+                return "emptycards/transparent";
+            case CardID.CardIndicatorRed:
+                return "emptycards/CardIndicatorRed";
+            case CardID.FieldIndicatorGreen:
+                return "emptycards/green";
+            case CardID.FieldIndicatorBlack:
+                return "emptycards/black";
+            case CardID.FieldIndicatorYellow:
+                return "emptycards/yellow";
         }
         return "cards/" + team + "/" + card;
 
+
+    }
+    static public string GetCardName(CardID cardid, int x, int y) {
+        switch (cardid) {
+            default:
+                return "Error " + x + "," + y;
+            case CardID.Card:
+                return "Card " + x + "," + y;
+            case CardID.Blankcard:
+                return "Card " + x + "," + y;
+            case CardID.Pointcard:
+                return "Card " + x + "," + y;
+            case CardID.Startpoint:
+                return "Card " + x + "," + y;
+            case CardID.Blockcard:
+                return "Card " + x + "," + y;
+            case CardID.FieldIndicator:
+                return "FieldIndicator " + x + "," + y;
+            case CardID.FieldIndicatorRed:
+                return "FieldIndicatorRed " + x + "," + y;
+            case CardID.Doublecard:
+                return "Doublecard " + x + "," + y;
+            case CardID.Deletecard:
+                return "Deletecard " + x + "," + y;
+            case CardID.Burncard:
+                return "Burncard " + x + "," + y;
+            case CardID.Infernocard:
+                return "Infernocard " + x + "," + y;
+            case CardID.Changecard:
+                return "Changecard " + x + "," + y;
+            case CardID.Cancercard:
+                return "Cancercard " + x + "," + y;
+            case CardID.HotPotatoe:
+                return "HotPotatoe " + x + "," + y;
+            case CardID.Nukecard:
+                return "Nukecard " + x + "," + y;
+            case CardID.Vortexcard:
+                return "Vortexcard " + x + "," + y;
+            case CardID.Anchorcard:
+                return "Anchorcard " + x + "," + y;
+            case CardID.Shufflecard:
+                return "Shufflecard " + x + "," + y;
+            case CardID.CardIndicator:
+                return "CardIndicator " + x + "," + y;
+        }
     }
 }

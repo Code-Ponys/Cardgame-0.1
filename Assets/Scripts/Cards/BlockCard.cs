7358c93e9867cfc1bb5285c15ac9953b56895aa0
CodeFoxByChojo <playhadde@gmail.com>
2017-08-15 16:28:02 +0200

    improved and added Blockcard

--- Assets/Scripts/Cards/BlockCard.cs
+++ Assets/Scripts/Cards/BlockCard.cs
@@ -7,36 +7,90 @@ namespace Cards {
 
     public class BlockCard : Card {
 
+        GameObject F;
+        GameObject CardIndicatorLeft;
+        GameObject CardIndicatorRight;
+        GameObject CardIndicatorUp;
+        GameObject CardIndicatorDown;
+        GameObject CardLeft;
+        GameObject CardRight;
+        GameObject CardUp;
+        GameObject CardDown;
+        GameObject FieldIndicatorLeft;
+        GameObject FieldIndicatorRight;
+        GameObject FieldIndicatorUp;
+        GameObject FieldIndicatorDown;
+
+
         public Block blockDirection;
+        private bool cardprocessdone;
 
         private void Start() {
-            GameObject IndicatorLeft = GameObject.Find("FieldIndicator " + (x - 1) + "," + y);
-            if (IndicatorLeft != null) {
-                SpriteRenderer rend1 = IndicatorLeft.GetComponent<SpriteRenderer>();
-                rend1.sprite = Resources.Load<Sprite>("emptycards/green");
-                IndicatorLeft.GetComponent<Indicator>().indicatorState = IndicatorState.reachable;
+            F = GameObject.Find("Field");
+            F.GetComponent<GameManager>().cardlocked = true;
+            CardIndicatorLeft = GameObject.Find(Slave.GetCardName(CardID.CardIndicator, x - 1, y));
+            CardIndicatorRight = GameObject.Find(Slave.GetCardName(CardID.CardIndicator, x + 1, y));
+            CardIndicatorUp = GameObject.Find(Slave.GetCardName(CardID.CardIndicator, x, y + 1));
+            CardIndicatorDown = GameObject.Find(Slave.GetCardName(CardID.CardIndicator, x, y - 1));
+            CardLeft = GameObject.Find(Slave.GetCardName(CardID.Card, x - 1, y));
+            CardRight = GameObject.Find(Slave.GetCardName(CardID.Card, x + 1, y));
+            CardUp = GameObject.Find(Slave.GetCardName(CardID.Card, x, y + 1));
+            CardDown = GameObject.Find(Slave.GetCardName(CardID.Card, x, y - 1));
+            FieldIndicatorLeft = GameObject.Find(Slave.GetCardName(CardID.FieldIndicator, x - 1, y));
+            FieldIndicatorRight = GameObject.Find(Slave.GetCardName(CardID.FieldIndicator, x + 1, y));
+            FieldIndicatorUp = GameObject.Find(Slave.GetCardName(CardID.FieldIndicator, x, y + 1));
+            FieldIndicatorDown = GameObject.Find(Slave.GetCardName(CardID.FieldIndicator, x, y - 1));
+            if (CardLeft == null && FieldIndicatorLeft != null
+                && FieldIndicatorLeft.GetComponent<Indicator>().team == Team.system
+                && FieldIndicatorLeft.GetComponent<Indicator>().currentcolor != IndicatorColor.red) {
+                CardIndicatorLeft.GetComponent<Indicator>().setColor(IndicatorColor.greencovered);
             }
-
-            GameObject IndicatorRight = GameObject.Find("FieldIndicator " + (x + 1) + "," + y);
-            if (IndicatorRight != null) {
-                SpriteRenderer rend2 = IndicatorRight.GetComponent<SpriteRenderer>();
-                rend2.sprite = Resources.Load<Sprite>("emptycards/green");
-                IndicatorRight.GetComponent<Indicator>().indicatorState = IndicatorState.reachable;
+            if (CardRight == null && FieldIndicatorLeft != null
+                && FieldIndicatorRight.GetComponent<Indicator>().team == Team.system
+                && FieldIndicatorRight.GetComponent<Indicator>().currentcolor != IndicatorColor.red) {
+                CardIndicatorRight.GetComponent<Indicator>().setColor(IndicatorColor.greencovered);
             }
-
-            GameObject IndicatorDown = GameObject.Find("FieldIndicator " + x + "," + (y - 1));
-            if (IndicatorDown != null) {
-                SpriteRenderer rend3 = IndicatorDown.GetComponent<SpriteRenderer>();
-                rend3.sprite = Resources.Load<Sprite>("emptycards/green");
-                IndicatorDown.GetComponent<Indicator>().indicatorState = IndicatorState.reachable;
+            if (CardUp == null && FieldIndicatorLeft != null
+                && FieldIndicatorUp.GetComponent<Indicator>().team == Team.system
+                && FieldIndicatorUp.GetComponent<Indicator>().currentcolor != IndicatorColor.red) {
+                CardIndicatorUp.GetComponent<Indicator>().setColor(IndicatorColor.greencovered);
             }
+            if (CardDown == null && FieldIndicatorLeft != null
+                && FieldIndicatorDown.GetComponent<Indicator>().team == Team.system
+                && FieldIndicatorDown.GetComponent<Indicator>().currentcolor != IndicatorColor.red) {
+                CardIndicatorDown.GetComponent<Indicator>().setColor(IndicatorColor.greencovered);
+            }
+        }
 
-            GameObject IndicatorUp = GameObject.Find("FieldIndicator " + x + "," + (y + 1));
-            if (IndicatorUp != null) {
-                SpriteRenderer rend4 = IndicatorUp.GetComponent<SpriteRenderer>();
-                rend4.sprite = Resources.Load<Sprite>("emptycards/green");
-                IndicatorUp.GetComponent<Indicator>().indicatorState = IndicatorState.reachable;
+        void Update() {
+            if (cardprocessdone) return;
+            if (Input.GetKeyDown(KeyCode.Mouse0)) {
+                if (F.GetComponent<GameManager>().cardlocked == true) {
+                    Vector3 mouseWorldPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+                    int indexX = F.GetComponent<Field>().RoundIt(mouseWorldPos.x);
+                    int indexY = F.GetComponent<Field>().RoundIt(mouseWorldPos.y);
+                    GameObject CardIndicator = GameObject.Find(Slave.GetCardName(CardID.CardIndicator, indexX, indexY));
+                    GameObject FieldIndicator = GameObject.Find(Slave.GetCardName(CardID.FieldIndicator, indexX, indexY));
+                    GameObject Card = GameObject.Find(Slave.GetCardName(CardID.Card, indexX, indexY));
+
+                    if (CardIndicator.GetComponent<Indicator>().placeallow == true && Card == null) {
+                        FieldIndicator.GetComponent<Indicator>().indicatorState = IndicatorState.blocked;
+                        FieldIndicator.GetComponent<Indicator>().team = F.GetComponent<GameManager>().currentPlayer;
+                        FieldIndicator.GetComponent<Indicator>().currentcolor = IndicatorColor.red;
+                        F.GetComponent<GameManager>().animationDone = true;
+                        F.GetComponent<GameManager>().cardlocked = false;
+                        cardprocessdone = true;
+                        print("Change to transparent");
+                        CardIndicatorLeft.GetComponent<Indicator>().setColor(IndicatorColor.transparent);
+                        CardIndicatorRight.GetComponent<Indicator>().setColor(IndicatorColor.transparent);
+                        CardIndicatorUp.GetComponent<Indicator>().setColor(IndicatorColor.transparent);
+                        CardIndicatorDown.GetComponent<Indicator>().setColor(IndicatorColor.transparent);
+
+
+                    }
+                }
             }
         }
     }
 }
+

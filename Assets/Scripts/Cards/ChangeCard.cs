3001445cf9907f0e9f7c14ff8834b0977e726106
CodeFoxByChojo <playhadde@gmail.com>
2017-08-16 11:57:39 +0200

    implemented Changecard

--- Assets/Scripts/Cards/ChangeCard.cs
+++ Assets/Scripts/Cards/ChangeCard.cs
@@ -2,15 +2,33 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ChangeCard : Card {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+namespace Cards {
+    public class ChangeCard : Card {
+
+        GameObject F;
+        GameObject Card;
+        GameObject Cardbelow;
+        SpriteRenderer SpriteRenderer;
+        private bool cardprocessdone;
+
+        // Use this for initialization
+        void Start() {
+            F = GameObject.Find("Field");
+            Card = GameObject.Find(Slave.GetCardName(CardID.Changecard, x, y));
+
+
+            Cardbelow = GameObject.Find(Slave.GetCardName(CardID.Card, x, y));
+            Cardbelow.GetComponent<Card>().cardid = CardID.Blankcard;
+            SpriteRenderer = Cardbelow.GetComponent<SpriteRenderer>();
+            SpriteRenderer.sprite = Resources.Load<Sprite>(Slave.GetImagePath(CardID.Blankcard, Cardbelow.GetComponent<Card>().team));
+            F.GetComponent<GameManager>().animationDone = true;
+
+            DestroyImmediate(Card);
+        }
+
+        // Update is called once per frame
+        void Update() {
+        }
+    }
 }

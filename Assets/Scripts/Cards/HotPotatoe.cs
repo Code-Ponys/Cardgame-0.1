a9ec4b18cd5a8391afc1a65f04b9453baacb4079
CodeFoxByChojo <playhadde@gmail.com>
2017-08-18 15:50:29 +0200

    Implemented HotPotatoe wrong. Fixed it.

--- Assets/Scripts/Cards/HotPotatoe.cs
+++ Assets/Scripts/Cards/HotPotatoe.cs
@@ -9,26 +9,44 @@ public class HotPotatoe : Card {
     GameObject HandCard3;
 
     // Use this for initialization
-    void Start () {
+    void Start() {
         GameObject F = GameObject.Find("Field");
 
         Team team = F.GetComponent<GameManager>().currentPlayer;
         if (team == Team.blue) {
-            GameObject.Find("HandCard1red").GetComponent<Handcards>().cardid = CardID.none;
-            GameObject.Find("HandCard2red").GetComponent<Handcards>().cardid = CardID.none;
-            GameObject.Find("HandCard3red").GetComponent<Handcards>().cardid = CardID.none;
-        }else {
-            GameObject.Find("HandCard1blue").GetComponent<Handcards>().cardid = CardID.none;
-            GameObject.Find("HandCard2blue").GetComponent<Handcards>().cardid = CardID.none;
-            GameObject.Find("HandCard3blue").GetComponent<Handcards>().cardid = CardID.none;
+            if (GameObject.Find("HandCard1red").GetComponent<Handcards>().cardid != CardID.Pointcard
+                || GameObject.Find("HandCard1red").GetComponent<Handcards>().cardid != CardID.Blankcard) {
+                GameObject.Find("HandCard1red").GetComponent<Handcards>().cardid = CardID.none;
+            }
+            if (GameObject.Find("HandCard2red").GetComponent<Handcards>().cardid != CardID.Pointcard
+                || GameObject.Find("HandCard2red").GetComponent<Handcards>().cardid != CardID.Blankcard) {
+                GameObject.Find("HandCard2red").GetComponent<Handcards>().cardid = CardID.none;
+            }
+            if (GameObject.Find("HandCard3red").GetComponent<Handcards>().cardid != CardID.Pointcard
+                || GameObject.Find("HandCard3red").GetComponent<Handcards>().cardid != CardID.Blankcard) {
+                GameObject.Find("HandCard3red").GetComponent<Handcards>().cardid = CardID.none;
+            }
+        } else {
+            if (GameObject.Find("HandCard1blue").GetComponent<Handcards>().cardid != CardID.Pointcard
+                || GameObject.Find("HandCard1blue").GetComponent<Handcards>().cardid != CardID.Blankcard) {
+                GameObject.Find("HandCard1blue").GetComponent<Handcards>().cardid = CardID.none;
+            }
+            if (GameObject.Find("HandCard2blue").GetComponent<Handcards>().cardid != CardID.Pointcard
+                || GameObject.Find("HandCard2blue").GetComponent<Handcards>().cardid != CardID.Blankcard) {
+                GameObject.Find("HandCard2blue").GetComponent<Handcards>().cardid = CardID.none;
+            }
+            if (GameObject.Find("HandCard3blue").GetComponent<Handcards>().cardid != CardID.Pointcard
+                || GameObject.Find("HandCard3blue").GetComponent<Handcards>().cardid != CardID.Blankcard) {
+                GameObject.Find("HandCard3blue").GetComponent<Handcards>().cardid = CardID.none;
+            }
         }
 
-        Destroy(GameObject.Find(Slave.GetCardName(CardID.HotPotatoe, x, y)));
+        F.GetComponent<GameManager>().RemoveCard(GameObject.Find(Slave.GetCardName(CardID.HotPotatoe, x, y)));
         F.GetComponent<GameManager>().animationDone = true;
     }
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+    // Update is called once per frame
+    void Update() {
+
+    }
 }
